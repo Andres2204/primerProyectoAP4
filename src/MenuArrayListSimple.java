6d492babb856c6efb8e014006377c136f9402010
DavidAntolinez <128693961+DavidAntolinez@users.noreply.github.com>
2024-02-27 14:07:40 -0500

    Add files via upload

--- src/MenuArrayListSimple.java
+++ src/MenuArrayListSimple.java
@@ -6,23 +6,55 @@ public class MenuArrayListSimple extends Menu {
 
     @Override
     public void menu() {
+        ArrayListSimple simple = new ArrayListSimple();
         while (true) {
-            String opt = input("Seleccione una opción:\n1. Menu 1\n2. Menu 2\n3. Menu 3\n4. Salir");
-            if (opt.equals("0")) {
-                msg("Saliendo del programa...");
+            String opt = input(
+                    "Seleccione una opción:\n1. Ingresar un elemento nuevo\n2. Ingresar un elemento nuevo en una poscicion especifica\n3. Mostrar\n4. Borrar Elemento\n5. Borrar el Elemento de una poscicion especifica\n6. Regresar");
+            if (opt == null || opt.equals("6")) {
+                msg("Regresando...");
                 break;
             }
             switch (opt) {
                 case "1":
                     // Lógica para la opción 1
-                    msg("Seleccionaste la opción 1");
+                    simple.llenar(input("Ingrese el elemento"));
                     break;
 
                 case "2":
                     // Lógica para la opción 2
-                    msg("Seleccionaste la opción 2");
+                    try {
+                        simple.Adicionar(input("Ingrese el elemento"),
+                                Integer.parseInt(input("Ingrese la posicion")) - 1);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        msg("Poscicion invalida");
+                    }
                     break;
 
+                case "3":
+                    // Lógica para la opción 3
+                    msg(simple.listar());
+                    break;
+
+                case "4":
+                    // Lógica para la opción 4
+                    try {
+                        simple.Belemento(input("Ingrese el elemento"));
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        msg("No se encuentra este elemento");
+                    }
+
+                    break;
+                case "5":
+                    // Lógica para la opción 5
+                    try {
+                        simple.BelementoPn(Integer.parseInt(input("Ingrese la poscicion")) - 1);
+                    } catch (Exception e) {
+                        // TODO: handle exception
+                        msg("Poscicion invalida");
+                    }
+                    break;
 
                 default: {
                     msg("Opción inválida. Por favor, elija otra opción.");
--- src/MenuPrincipal.java
+++ src/MenuPrincipal.java
@@ -7,24 +7,28 @@ public class MenuPrincipal extends Menu {
     @Override
     public void menu() {
         while (true) {
-            String opt = input("Seleccione una opción:\n1. Menu 1\n2. Menu 2\n3. Menu 3\n4. Salir");
-            if (opt.equals("4")) {
+            String opt = input("Seleccione una opción:\n1. Menu ArrayList Simple\n2. Menu ArrayList Objeto\n3. Menu ArrayList ArrayList\n4. Salir");
+            if (opt == null || opt.equals("4")) {
                 msg("[!] Saliendo del programa...");
                 System.exit(0);
             }
             switch (opt) {
                 case "1":
-                    msg("llendo el menu 1");
+                    msg("llendo el menu de ArrayList Simple");
                     MenuArrayListSimple mas = new MenuArrayListSimple("Menu ArrayList Simple");
                     mas.menu();
                     break;
 
                 case "2":
-                    msg("llendo el menu 2");
+                    msg("llendo el menu de ArrayList Objeto");
+                    MenuPartidos menos = new MenuPartidos("Menu Partidos");
+                    menos.menu();
                     break;
 
                 case "3":
-                    msg("llendo el menu 3");
+                    msg("llendo el menu ArrayList ArrayList");
+                    MenuArrayMatriz mul = new MenuArrayMatriz("Menu ArrayList ArrayList");
+                    mul.menu();
                     break;
 
                 default:
